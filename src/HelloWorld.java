0a4723f633ea4d8abb4ae1d9ed47d3f5184aaa62
DONG Sophie <12200554@crit-xubuntu>
2023-03-30 15:58:41 +0200

    Arbitré

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,13 @@
-
 public class HelloWorld {
-
+ 
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-		System.out.println("Hello World !");
+<<<<<<< HEAD
+		System.out.println("Olá mundo !");
+=======
+		System.out.println("Hallo Welt !");
+>>>>>>> 1a2a6ff8034f79346f5e296400f520b086e9d9ea
 		System.out.println("Hola Mundo !");
 	}
-
+ 
 }
