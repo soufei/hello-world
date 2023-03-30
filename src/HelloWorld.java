bd5d71762726f497e456955de8f6066394de3278
DONG Sophie <12200554@crit-xubuntu>
2023-03-30 15:55:56 +0200

    nouveau fic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Ciao mondo !");
 		System.out.println("Hello World !");
+		System.out.println("Hola Mundo !");
 	}
 
 }
